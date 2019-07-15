0d337985385c2fd55469518146c1aa4908ef1a6e
Eli Sadoff <eli_sadoff@comcast.com>
2019-07-15 10:45:48 -0400

    Debug test failures

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -7,19 +7,21 @@ RSpec.describe TracksController, type: :controller do
     before do
       @track_count = rand 1..100
       @track_count.times { create :track }
-      get :index, format: :json
-      @json = JSON.parse response.body
-      @first_track = @json.first
     end
 
     it 'should return the appropriate number of tracks with appropriate attributes' do
-      expect(@json.size).to eq @track_count
-      expect(@first_track).to have_key 'artist'
-      expect(@first_track).to have_key 'album'
-      expect(@first_track).to have_key 'listened_at'
-      expect(@first_track).to have_key 'name'
+      get :index, format: :json
+      json = JSON.parse response.body
+      first_track = json.first
+      expect(json.size).to eq @track_count
+      expect(first_track).to have_key 'artist'
+      expect(first_track).to have_key 'album'
+      expect(first_track).to have_key 'listened_at'
+      expect(first_track).to have_key 'name'
     rescue JSON::ParserError
-      File.write 'response.html', response.body
+      # Test should fail on rescue
+      expect(false).to be_truthy
+      File.write 'response_index.html', response.body
     end
   end
 
@@ -27,18 +29,23 @@ RSpec.describe TracksController, type: :controller do
     attributes = %w[artist name album]
     (1..attributes.size).each do |size|
       attributes.permutation(size).each do |attrs|
-        @action = "by_#{attrs.join '_and_'}"
-        context @action do
+        action = "by_#{attrs.join '_and_'}"
+        context action do
           before do
             rand(1..100).times { create :track }
-            get @action.to_sym, format: :json
-            @first_track = @json.first
           end
 
           it 'should return the appropriate attributes' do
+            get action.to_sym, format: :json
+            json = JSON.parse response.body
+            @first_track = json.first
             attrs.each do |attr|
               expect(@first_track).to have_key attr
             end
+          rescue JSON::ParserError
+            # Test should fail on rescue
+            expect(false).to be_truthy
+            File.write "response_#{action}.html", response.body
           end
         end
       end
