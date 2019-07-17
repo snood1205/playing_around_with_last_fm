7a4108c3caacca5b3f3e110b7eee8f7a0adfb43a
Eli Sadoff <eli_sadoff@comcast.com>
2019-07-17 12:29:56 -0400

    Add url and image url for greater last.fm integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_121554) do
+ActiveRecord::Schema.define(version: 2019_07_17_161720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2019_07_16_121554) do
     t.datetime "listened_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
+    t.string "image_url"
   end
 
 end
