13bf04d62031c207d033723a140c3d16cc639a76
Eli Sadoff <eli_sadoff@comcast.com>
2019-07-16 22:20:43 -0400

    Add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@
 # Base controller for application. Any logic put in here applies to all controllers which are a subclass of
 # `ApplicationController`, which includes a majority, if not all, of the controllers.
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
+  helper_method :track_by_actions
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def track_by_actions
+    TracksController::VALID_BY_ACTIONS
+  end
 end
+
