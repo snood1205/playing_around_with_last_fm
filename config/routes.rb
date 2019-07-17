893da19dd6ee017a903ea127c792b268e533b97a
Eli Sadoff <eli_sadoff@comcast.com>
2019-07-17 19:21:33 -0400

    Play around with instance_variable_set and instance_variable_get
    (I definitely could've used a hash but this was more fun)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tracks, except: :show
+  attributes = %w[artist name album]
+
+  resources :tracks, except: :show do
+    attributes.each { |attr| get attr }
+  end
   resource :tracks do
     # Initialize all the by_artist_and_etc endpoints
-    attributes = %w[artist name album]
     (1..attributes.size).each do |size|
       attributes.permutation(size).each do |attrs|
         get :"by_#{attrs.join '_and_'}"
