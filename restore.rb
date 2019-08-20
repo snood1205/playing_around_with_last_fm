238aaef44b6225e9752eb4658d864a97725d33c2
Eli Sadoff <eli.sadoff@dockyard.com>
2019-08-20 14:43:45 -0400

    fix restore

--- restore.rb
+++ restore.rb
@@ -1,4 +1,4 @@
-tracks = JSON.parse File.read 'load.json'
+tracks = JSON.parse File.read 'tracks.json'
 tracks.each do |track|
   Track.create %w[artist album name listened_at url image_url].map {|i| [i, track[i]]}.to_h
 end
